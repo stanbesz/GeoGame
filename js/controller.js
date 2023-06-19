de56b54d583de626566ab3ad311f672beffe40c1
Stanimir Aleksandrov <stanimiralexandrov@gmail.com>
2023-06-19 18:59:07 +0300

    Added buttons, .gitignore and more

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,11 @@
 'using strict';
 
+const commentSection = document.querySelector('.comment-section');
+
+const removeLastCommentBorder = function(){
+    const lastComment = commentSection.lastElementChild;
+    lastComment.style.borderBottom = "none";
+    console.log(lastComment);
+}
+removeLastCommentBorder();
 console.log('Initial setup');
